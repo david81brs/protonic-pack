28b3ce1b11a152e6af1a7c99b41b7fb716eaff65
David Borges <david81br@gmail.com>
2020-01-06 19:30:19 -0300

    Fix hide and show column

--- static/js/proto.js
+++ static/js/proto.js
@@ -1,10 +1,12 @@
 $("document").ready(function() {
   $("#toggler2").click(function() {
     $("#left-form").hide();
+    $("#right-list").css("grid-column","1/3");
   });
 
   $("#toggler1").click(function() {
     $("#left-form").fadeIn(100);
+    $("#right-list").css("grid-column","2/3");
   });
   
 });
